61cc4e7d660d1a39a814f4d9e2537e8ae2f2a973
Geometer1729 <kuhnsbt@clarkson.edu>
2018-07-12 13:19:27 -0400

    Re Organized FuncToIO

--- Haskell/Util/FuncToIO.hs
+++ Haskell/Util/FuncToIO.hs
@@ -10,21 +10,6 @@ import           System.Process
 
 data MaybeFuncTo a = FuncTo (String -> MaybeFuncTo a) | Plain a
 
-app :: MaybeFuncTo a -> [String] -> a
-app (FuncTo f) (x:xs) = app (f x) xs
-app (Plain x) []      = x
-app _ []              = error "Not enough arguments"
-app (Plain _ ) _      = error "Too many arguments"
-
-class Cast a where
-    cast :: a -> MaybeFuncTo String
-
-instance (Read a, Cast b) => Cast ( a -> b ) where
-    cast f = FuncTo (cast . f . read)
-
-instance Show a => Cast a where
-    cast x = Plain (show x)
-
 funcToExe :: (Cast a) => a-> IO ()
 funcToExe f = do
     args <- getArgs
@@ -41,16 +26,20 @@ funcToPlot f = do
       args <- getArgs
       app (castPlot f) args
 
-plotListCode::String
-plotListCode = concat [ x ++ "\n" | x<- xs]
-  where
-    xs = ["import matplotlib.pyplot as plt" , "import numpy as np",    "from mpl_toolkits.mplot3d import Axes3D",  teribleString ,  "out= (''.join ( [c for line in file for c in line] [1:-1] ))" ,  "x = [float(x) for x in out.split(',')]",  "file.close()",  "plt.plot(range(0,len(x)),x)", "plt.show()" ] ::[String]
-    teribleString = "file = open(\"data.txt\",)" :: String
-plotListTupCode::String
-plotListTupCode = concat [ x ++ "\n" | x<- xs]
-  where
-    xs = [" import matplotlib.pyplot as plt" , "import numpy as np", "from mpl_toolkits.mplot3d import Axes3D" , teribleString , "out= (''.join ( [c for line in file for c in line] [2:-2] ))" , "x = np.transpose ( [ [ float(p) for p in tup.split(',') ] for tup in out.split('),(')] )" , "print (x)" , "file.close()" , "plt.plot(x[0],x[1])" , "plt.show()" ]
-    teribleString = "file = open(\"data.txt\",)"
+app :: MaybeFuncTo a -> [String] -> a
+app (FuncTo f) (x:xs) = app (f x) xs
+app (Plain x) []      = x
+app _ []              = error "Not enough arguments"
+app (Plain _ ) _      = error "Too many arguments"
+
+class Cast a where
+    cast :: a -> MaybeFuncTo String
+
+instance (Read a, Cast b) => Cast ( a -> b ) where
+    cast f = FuncTo (cast . f . read)
+
+instance Show a => Cast a where
+    cast x = Plain (show x)
 
 class Plotable a where
   castPlot:: a -> MaybeFuncTo (IO ())
@@ -65,9 +54,21 @@ instance Plotable [Double] where
     writeFile "plotfile.py" plotListCode
     callCommand "python plotfile.py" )
 
+plotListCode::String
+plotListCode = concat [ x ++ "\n" | x<- xs]
+  where
+    xs = ["import matplotlib.pyplot as plt" , "import numpy as np",    "from mpl_toolkits.mplot3d import Axes3D",  teribleString ,  "out= (''.join ( [c for line in file for c in line] [1:-1] ))" ,  "x = [float(x) for x in out.split(',')]",  "file.close()",  "plt.plot(range(0,len(x)),x)", "plt.show()" ] ::[String]
+    teribleString = "file = open(\"data.txt\",)" :: String
+
 instance Plotable [(Double,Double)] where
   castPlot dat = Plain ( do
     writeFile "Data.txt" (show dat)
     putStrLn "On to python"
     writeFile "plotfile.py" plotListTupCode
     callCommand "python plotfile.py" )
+
+plotListTupCode::String
+plotListTupCode = concat [ x ++ "\n" | x<- xs]
+  where
+    xs = [" import matplotlib.pyplot as plt" , "import numpy as np", "from mpl_toolkits.mplot3d import Axes3D" , teribleString , "out= (''.join ( [c for line in file for c in line] [2:-2] ))" , "x = np.transpose ( [ [ float(p) for p in tup.split(',') ] for tup in out.split('),(')] )" , "print (x)" , "file.close()" , "plt.plot(x[0],x[1])" , "plt.show()" ]
+    teribleString = "file = open(\"data.txt\",)"
