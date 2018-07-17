6d193690b60e971030017914ce55be72b7fe7c0f
Geometer1729 <kuhnsbt@clarkson.edu>
2018-07-17 15:28:09 -0400

    combined Dynamify into Norm

--- Haskell/Util/Norm.hs
+++ Haskell/Util/Norm.hs
@@ -1,4 +1,4 @@
-module Haskell.Util.Norm where
+module Haskell.Util.Norm (numLim,dynamify) where
 
 class Normed a where
   norm :: a -> a -> Double
@@ -14,3 +14,13 @@ numLim eps xs = head final
   where
     pairs = zipWith (\x y -> (x,y,norm x y) ) xs (tail xs)
     final = [y | (x,y,n) <- pairs , n < eps]
+
+dynamify:: Normed b =>  (a -> Double ->  b) -> a -> Double ->  b
+dynamify func input tol = rollingDynamify func tol tol input
+
+rollingDynamify:: Normed b => (a -> Double ->  b) -> Double -> a ->  Double -> b
+rollingDynamify func tol try input = if dist < tol then next else rollingDynamify func tol (try/2) input
+  where
+    this = func input try
+    next = func input (try/2)
+    dist = norm this next
